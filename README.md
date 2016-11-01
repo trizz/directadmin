2a54579ab704eb3b69c08fd8b15ac4371e02af71
Tristan <me@trizz.io>
2016-11-01 22:31:12 +0100

    Add examples and readme

---
+++ README.md
@@ -0,0 +1,56 @@
+## DirectAdmin
+
+This library provides a simple abstract DirectAdmin class that can be used to create (simple) "components" to consume the DirectAdmin API.
+
+### How to use
+
+There are two ways to use this library:
+
+**Without [composer](https://getcomposer.org/):**
+```php
+<?php
+// Include the required files. "DirectAdmin.php" is ALWAYS required.
+require_once '/path/to/library/src/DirectAdmin.php';
+// After including the base class, you can load the components individually.
+require_once '/path/to/library/src/MailingList.php';
+
+// Initialize the component.
+$daMailingList = new \Trizz\DirectAdmin\<COMPONENT>('http://<host>', '<username>', '<password>', '<domain>');
+```
+
+**With [composer](https://getcomposer.org/):**
+```php
+<?php
+// Make sure that the composer autoload file is loaded. In most cases this is already handled by your app.
+
+// Initialize the component.
+$daMailingList = new \Trizz\DirectAdmin\<COMPONENT>('http://<host>', '<username>', '<password>', '<domain>');
+```
+
+### Examples
+There's a directory called `/examples` that contains some example PHP files. 
+
+### Available "components"
+Below are the currently available classes.
+
+#### General methods
+There are some several methods that are available in every component.
+
+| Method | Description
+| --- | ---
+| `setDomain($domain)` | Update the domain to use. Can be used to switch to another domain with the same user credentials.
+
+#### MailingList
+This component allows you to manage the available mailing lists inside DirectAdmin.
+Available methods:
+
+| Method | Description
+| --- | ---
+| `lists()` | Get an overview of the available mailing lists.
+| `addList($name)` | Add a new mailing list.
+| `deleteList($name)` | Remove a mailing list.
+| `getSubscribers($listName, $subscriberType = all)` | Get all subscribers for the specified list. The `$subscriberType` can be: subscribers or digest_subscribers. By default both types are returned.
+| `addAddress($address, $list, $type = 'list')` | Add a single email address to the specified mailing list. `$type` Can be "list" or "digest". Default: list.
+| `addAddresses($addresses, $list, $type = 'list')` | Add multiple email addresses to the specified mailing list. `$type` Can be "list" or "digest". Default: list.
+| `deleteAddress($address, $list, $type = 'list')` | Delete an email address from the specified list. `$type` Can be "list" or "digest". Default: list.
+| `deleteAddresses($addresses, $list, $type = 'list')` | Delete multiple email addresses from the specified mailing list. `$type` Can be "list" or "digest". Default: list.
---
+++ examples/mailing_list.php
@@ -0,0 +1,92 @@
+<?php
+// Include the required files.
+require_once '../src/DirectAdmin.php';
+require_once '../src/MailingList.php';
+
+// Initialize the mailing list class.
+$daMailingList = new \Trizz\DirectAdmin\MailingList('http://<host>', '<username>', '<password>', '<domain>');
+
+// Use this domain suffix when generating random email addresses.
+$testEmailSuffix = '@example.com';
+
+// Use this name to create a "test" mailing list.
+$testMailingList = 'api'.uniqid();
+
+/******************************\
+|    Run the tests/examples    |
+\******************************/
+
+// Create a 'test' mailing list.
+$daMailingList->addList($testMailingList);
+
+// Get all available mailing lists.
+$availableLists = $daMailingList->lists();
+// Loop through each list.
+echo bold("Available lists:\n");
+foreach ($availableLists as $list) {
+    // Show the list name.
+    echo sprintf("  %s (subscribers: %d / digest subscribers: %d)\n", bold($list['name']), $list['subscribers'], $list['digest_subscribers']);
+    $subscribers = $daMailingList->getSubscribers($list['name']);
+
+    // List the regular subscribers.
+    echo bold(sprintf("    Subscribers in '%s':\n", $list['name']));
+    foreach ($subscribers['subscribers'] as $member) {
+        echo sprintf("        %s\n", $member);
+    }
+
+    // List the digest subscribers.
+    echo bold(sprintf("    Digest subscribers in '%s':\n", $list['name']));
+    foreach ($subscribers['digest_subscribers'] as $member) {
+        echo sprintf("        %s\n", $member);
+    }
+
+    // Only proceed when the list name is "directAdminApiTest" because we're adding/removing new addresses!
+    if ($list['name'] === $testMailingList) {
+        // Generate a single and an array with multiple email addresses.
+        $randomAddress = uniqid().$testEmailSuffix;
+        $randomAddresses = [uniqid().$testEmailSuffix, uniqid().$testEmailSuffix, uniqid().$testEmailSuffix];
+
+        // Test adding a single email address as subscriber.
+        echo bold(sprintf("    Add a single address to the list '%s'.\n", $list['name']));
+        echo '        '.$daMailingList->addAddress($randomAddress, $list['name'])['text']."\n";
+
+        // Test adding multiple email addresses as subscribers.
+        echo bold(sprintf("    Add multiple addresses to the list '%s'.\n", $list['name']));
+        echo '        '.$daMailingList->addAddresses($randomAddresses, $list['name'])['text']."\n";
+
+        // Test adding a single email address as digest subscriber.
+        echo bold(sprintf("    Add a single digest address to the list '%s'.\n", $list['name']));
+        echo '        '.$daMailingList->addAddress($randomAddress, $list['name'], 'digest')['text']."\n";
+
+        // Test adding multiple email addresses as digest subscribers.
+        echo bold(sprintf("    Add multiple digest addresses to the list '%s'.\n", $list['name']));
+        echo '        '.$daMailingList->addAddresses($randomAddresses, $list['name'], 'digest')['text']."\n";
+
+        // Test removing a single email address as subscriber.
+        echo bold(sprintf("    Delete a single address from the list '%s'.\n", $list['name']));
+        echo '        '.$daMailingList->deleteAddress($randomAddress, $list['name'])['text']."\n";
+
+        // Test removing multiple email addresses as subscriber.
+        echo bold(sprintf("    Delete multiple addresses from the list '%s'.\n", $list['name']));
+        echo '        '.$daMailingList->deleteAddresses($randomAddresses, $list['name'])['text']."\n";
+
+        // Test removing a single email address as digest subscriber.
+        echo bold(sprintf("    Delete a single digest address from the list '%s'.\n", $list['name']));
+        echo '        '.$daMailingList->deleteAddress($randomAddress, $list['name'], 'digest')['text']."\n";
+
+        // Test removing multiple email addresses as digest subscriber.
+        echo bold(sprintf("    Delete multiple digest addresses from the list '%s'.\n", $list['name']));
+        echo '        '.$daMailingList->deleteAddresses($randomAddresses, $list['name'], 'digest')['text']."\n";
+    }
+}
+
+// Remove the test mailing list.
+$daMailingList->deleteList($testMailingList);
+
+// Few newlines.
+echo "\n\n";
+
+// Little helper to format bold text.
+function bold($text) {
+    return sprintf("\033[1m%s\033[0m", $text);
+}
